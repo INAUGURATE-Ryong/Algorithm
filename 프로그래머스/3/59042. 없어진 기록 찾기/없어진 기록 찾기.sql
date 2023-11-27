0ded5170567caa8e9ebde1490a98dbffdfdc443c
INAUGURATE-Ryong <62015109+INAUGURATE-Ryong@users.noreply.github.com>
2023-11-27 18:43:33 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -2,5 +2,5 @@
 select o.ANIMAL_ID,o.NAME
 from ANIMAL_INS i right join ANIMAL_OUTS o
 on i.ANIMAL_ID = o.ANIMAL_ID
-where i.name is null and o.name is not null
+where i.ANIMAL_ID is null 
 -- order by name
