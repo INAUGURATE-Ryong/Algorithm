15a4036734e8d8b3eeca5d85f4099d51cc35c623
INAUGURATE-Ryong <62015109+INAUGURATE-Ryong@users.noreply.github.com>
2023-11-29 16:15:43 +0900

    [Bronze I] Title: 세로읽기, Time: 200 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        char[][] arr1 = new char[5][15];
+        String str = "";
+
+        for (int i = 0; i < arr1.length; i++) {
+            str = sc.next();
+            for (int j = 0; j < str.length(); j++) {
+                arr1[i][j] = str.charAt(j);
+            }
+        }
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 5; j++) {
+                if(arr1[j][i] != '\0'){
+                System.out.print(arr1[j][i]);}
+            }
+        }
+    }
+}
+
+
