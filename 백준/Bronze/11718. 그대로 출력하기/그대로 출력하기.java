00ed654bfeff572c99ab46f65af69c19a5d1508b
INAUGURATE-Ryong <62015109+INAUGURATE-Ryong@users.noreply.github.com>
2023-12-01 16:40:11 +0900

    Update 그대로 출력하기.java

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while(sc.hasNextLine()){
+        while(sc.hasNextLine()){  // hasNextLine 입력 소스에서 다음에 읽을 문자열이 있는지 여부를 확인하여 없으면 false,있으면 true를 반환
               System.out.println(sc.nextLine());
         }
       
