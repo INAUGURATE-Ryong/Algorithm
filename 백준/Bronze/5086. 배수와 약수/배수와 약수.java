df3edaf491e07b5d9fdd75118ed59638fe51343f
INAUGURATE-Ryong <62015109+INAUGURATE-Ryong@users.noreply.github.com>
2023-12-04 16:06:03 +0900

    [Bronze III] Title: 배수와 약수, Time: 204 ms, Memory: 17496 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            if(x==0 && y==0) break;
+            if (x > y) {
+                if (x % y == 0) {
+                    System.out.println("multiple");
+                } else {
+                    System.out.println("neither");
+                }
+            } else if (y > x) {
+                if (y % x == 0) {
+                    System.out.println("factor");
+                } else {
+                    System.out.println("neither");
+                }
+            }
+
+        }
+    }
+
+
+}
