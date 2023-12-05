6883cf0ba72bb6cfd5bb189f85654a6094a3f2a0
INAUGURATE-Ryong <62015109+INAUGURATE-Ryong@users.noreply.github.com>
2023-12-05 20:42:47 +0900

    Update 소트인사이드.java

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        char[] arr1 = sc.nextLine().toCharArray();
+        char[] arr1 = sc.nextLine().toCharArray(); // 문자열을 문자로 받아 배열에 담기.
         Arrays.sort(arr1);
         for (int i = arr1.length-1; i >=0; i--) {
             System.out.print(arr1[i]);
