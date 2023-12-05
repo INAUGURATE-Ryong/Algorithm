9f6229b306dbcf5e8f8d4dbba2e9d5000cc244d3
INAUGURATE-Ryong <62015109+INAUGURATE-Ryong@users.noreply.github.com>
2023-12-05 20:42:08 +0900

    [Silver V] Title: 소트인사이드, Time: 200 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        char[] arr1 = sc.nextLine().toCharArray();
+        Arrays.sort(arr1);
+        for (int i = arr1.length-1; i >=0; i--) {
+            System.out.print(arr1[i]);
+
+        }
+    }
+
+
+}
+
