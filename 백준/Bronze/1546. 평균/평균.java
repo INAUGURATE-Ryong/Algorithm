987114616583185f748fbbc5bd56da2a11530769
INAUGURATE-Ryong <62015109+INAUGURATE-Ryong@users.noreply.github.com>
2024-01-05 19:21:47 +0900

    [Bronze I] Title: 평균, Time: 268 ms, Memory: 18516 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,27 +1,30 @@
-import java.util.Arrays;
+import java.io.*;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        //System.setIn(new FileInputStream("C:\\Users\\awya5\\Desktop\\수업자료\\4_Java/input.txt"));
         Scanner sc = new Scanner(System.in);
-        int m = sc.nextInt();
-        double sum = 0;
-        double temp = 0;
-        double[] arr = new double[m];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = sc.nextInt();
+        int N = sc.nextInt();
+        int M[] = new int[N];
+
+        for (int i = 0; i < M.length; i++) {
+            M[i] = sc.nextInt();
         }
-        Arrays.sort(arr);
-        for (int i = 0; i < arr.length ; i++) {
-            arr[i] = arr[i] / arr[m - 1] * 100;
-            sum += arr[i];
-            temp =sum / m;
+        long sum = 0;
+        long max = 0;
+
+        for (int i = 0 ; i<N;i++){
+            if(M[i] > max) max = M[i];
+            sum += M[i];
 
         }
-
-        System.out.println(temp);
-
-
+        System.out.println(sum*100.0/max/N);
     }
 
+
+
+
 }
+
